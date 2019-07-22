ce6c23224b7364cb83a6858db55d86302f307b11
wongband <bryanwong7788@gmail.com>
2019-07-22 15:19:09 -0700

    Debug

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flaskblog import routes
 
 app = Flask(__name__)
 
@@ -7,4 +8,3 @@ app.config['SECRET_KEY'] = '47cece3f2555fc43ba70ef78cdf4321a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-from flaskblog import routes
